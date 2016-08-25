b16fba3a05fd6b653cb18f9819dfbd71150b46f4
Charlie Baxter and patrick lipscomb <charlie+patrick@>
2016-08-25 16:47:48 -0700

    work in progress on displaying average ratings

--- app/routes/all-feedback.js
+++ app/routes/all-feedback.js
@@ -2,13 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return Ember.RSVP.hash({
-      products: this.store.findAll('product'),
-      feedbacks: this.store.findAll('feedback')
-    });
+    return this.store.findAll('product')
   },
 
   actions: {
 
-  }
+  },
 });
